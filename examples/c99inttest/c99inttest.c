8bd25cfac08fabf07e7208fd66a8b1066c7b9238
zyl910 <zyl910@sina.com>
2013-07-31 22:48:45 +0800

    MIT License (MIT许可证).

--- examples/c99inttest/c99inttest.c
+++ examples/c99inttest/c99inttest.c
@@ -1,3 +1,22 @@
+//Copyright (c) 2000-2013 zyl910 <zyl910hero@gmail.com>
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
 
 /** @addtogroup GROUP_ZYLLIBC	zyllibc
  * @{
@@ -13,7 +32,7 @@
  * @author	[zyl910](mailto:zyl910hero@gmail.com)
  * @since	@ref GROUP_C99INT 1.0
  * @version	1.2
- * @date	2013-07-30
+ * @date	2013-07-31
  *
  * Links:
  *
@@ -25,7 +44,7 @@
  *
  * ## Change history (变更日志)
  *
- * [2013-07-30] v1.2
+ * [2013-07-31] v1.2
  *
  * * Using @ref GROUP_C99INT v1.2 .
  * * Using Doxygen style comments (使用doxygen规范注释).
--- src/auto_stdint.h
+++ src/auto_stdint.h
@@ -1,3 +1,22 @@
+//Copyright (c) 2000-2013 zyl910 <zyl910hero@gmail.com>
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
 
 /** @addtogroup GROUP_ZYLLIBC	zyllibc
  * [zyl910](https://github.com/zyl910/c99int)'s C/C++ librarys.
@@ -9,7 +28,7 @@
  *
  * @author	[zyl910](mailto:zyl910hero@gmail.com)
  * @version	1.2
- * @date	2013-07-30
+ * @date	2013-07-31
  *
  * Links:
  *
@@ -19,7 +38,7 @@
  *
  * ## Change history (变更日志)
  *
- * [2013-07-30] v1.2
+ * [2013-07-31] v1.2
  *
  * * Upload the code to [github](https://github.com/zyl910/c99int) (将代码上传到github).
  * * Change the directory structure (调整目录结构).
